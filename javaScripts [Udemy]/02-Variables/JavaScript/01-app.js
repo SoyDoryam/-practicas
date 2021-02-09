62440b5253f22dbd7b129a9c1cba5a4f6d0bf56c
dorian <shinagamialastor@gmail.com>
2021-02-09 00:11:55 -0600

    progrso var

---
+++ javaScripts [Udemy]/02-Variables/JavaScript/01-app.js
@@ -0,0 +1,20 @@
+//todo lo que debes de sarber sobre la variable var
+
+//iniciaizar una variable con un valor 
+var producto = "Monitor de 20 pulgadas"; 
+console.log(producto);
+
+//las variables se pueden reasignar
+producto = 'Monitor de 19 pulgadas';
+console.log(producto);
+
+//JavaScript es un lengaje de tipo dinamico, no s espesifica el tipo
+producto = 20;
+console.log(producto);
+
+// se puede inicializar son valor y asignarlo despues
+var disponible;
+
+disponible = true;
+
+disponible = false;
